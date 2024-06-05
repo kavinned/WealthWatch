513f6fa93290bb61a0ffbef2825cee96401a525f
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-06-05 13:41:23 +0800

    feat: create budget and transaction pages

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,21 +5,26 @@ import { getServerSession } from "next-auth";
 import { FaPiggyBank } from "react-icons/fa";
 
 export default async function Dashboard() {
-	// await new Promise((resolve) => setTimeout(resolve, 500));
 	const session = await getServerSession();
 
 	return (
 		<div className="flex flex-col justify-center w-screen h-[90vh]">
 			<span className="flex flex-row items-center justify-between">
-				<h1 className="text-2xl font-semibold text-zinc-700 ml-5 mt-5">
+				<h1
+					className="text-2xl font-semibold text-zinc-700 ml-5 mt-5 drop-shadow-lg"
+					style={{ textShadow: "0px 4px 4px rgba(0, 0, 0, 0.4)" }}
+				>
 					Welcome to your <br /> dashboard
 				</h1>
-				<h2 className="dash-name text-emerald-800 font-bold text-2xl mr-5 mt-5 flex flex-col justify-around items-center ">
+				<h2
+					style={{ textShadow: "0px 4px 4px rgba(0, 0, 0, 0.4)" }}
+					className="dash-name text-emerald-800 font-bold text-2xl mr-5 mt-5 flex flex-col justify-around items-center "
+				>
 					{session?.user?.name}
 					<FaPiggyBank />
 				</h2>
 			</span>
-			<div className="w-full h-full flex items-center justify-center gap-10">
+			<div className="w-full h-full flex items-center justify-center gap-10 sm:p-10 xs:p-10">
 				<BudgetCard session={session} />
 				<StockCard session={session} />
 			</div>
