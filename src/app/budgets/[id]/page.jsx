5c2f2c471057a040580bed8f291fbaecaa10669c
Kavin <kavin.ned@gmail.com>
2024-06-06 23:06:46 +0800

    feat: add budget detail page and add transaction form

--- src/app/budgets/[id]/page.jsx
+++ src/app/budgets/[id]/page.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { getServerSession } from "next-auth";
 
-export default function Budget() {
-	return <div>page</div>;
+async function fetchBudget(email, id) {
+	const res = await fetch(
+		`${process.env.NEXTAUTH_URL}/api/users/${email}/budgets/${id}`
+	);
+	return res?.json();
+}
+export default async function Budget({ params }) {
+	const { id } = params;
+	const session = await getServerSession();
+	const budget = await fetchBudget(session?.user?.email, id);
+
+	return (
+		<div>
+			{budget.name} {budget.limit}
+			<ul>
+				{budget.transactions.map((transaction) => (
+					<li key={transaction._id}>{transaction.description}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
--- src/app/budgets/[id]/transactions/new/page.jsx
+++ src/app/budgets/[id]/transactions/new/page.jsx
@@ -1,5 +1,67 @@
+"use client";
 import React from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-export default function AddTransactionPage() {
-	return <div>page</div>;
+export default function AddTransactionPage({ params }) {
+	const router = useRouter();
+	const { id } = params;
+	const { data: session } = useSession();
+	const email = session?.user?.email;
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		const formData = new FormData(event.target);
+		const name = formData.get("name");
+		const category = formData.get("category");
+		const amount = formData.get("amount");
+		const description = formData.get("description");
+		const date = formData.get("date");
+		const type = formData.get("type");
+
+		try {
+			const res = await fetch(
+				`/api/users/${email}/budgets/${id}/transactions`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+					},
+					method: "POST",
+					body: JSON.stringify({
+						name,
+						category,
+						amount,
+						description,
+						date,
+						type,
+					}),
+				}
+			);
+			await res.json();
+			router.push(`/budgets/${id}`);
+			router.refresh();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className="flex justify-center w-screen h-[90vh]">
+			<form className="auth w-1/3" onSubmit={handleSubmit}>
+				<label htmlFor="name">Name</label>
+				<input type="text" name="name" id="name" />
+				<label htmlFor="category">Category</label>
+				<input type="text" name="category" id="category" />
+				<label htmlFor="amount">Amount</label>
+				<input type="number" name="amount" id="amount" />
+				<label htmlFor="description">Description</label>
+				<input type="text" name="description" id="description" />
+				<label htmlFor="date">Date</label>
+				<input type="date" name="date" id="date" />
+				<label htmlFor="type">Type</label>
+				<input type="text" name="type" id="type" />
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
