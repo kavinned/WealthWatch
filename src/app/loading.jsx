61f438c99bff9d8b23f36c1c941b5e3a2ca71b9e
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-06-06 09:58:37 +0800

    fix: change class to classname

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Loading() {
 	return (
-		<div class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 scale-50">
-			<div class="p-4 bg-gradient-to-tr animate-spin from-green-500 to-blue-500 via-purple-500 rounded-full">
-				<div class="bg-white rounded-full">
-					<div class="w-24 h-24 rounded-full"></div>
+		<div className="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 scale-50">
+			<div className="p-4 bg-gradient-to-tr animate-spin from-green-500 to-blue-500 via-purple-500 rounded-full">
+				<div className="bg-white rounded-full">
+					<div className="w-24 h-24 rounded-full"></div>
 				</div>
 			</div>
 		</div>
