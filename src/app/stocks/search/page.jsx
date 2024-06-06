ce5661451819b753ba702be12e5537aca4df3b09
Kavin <kavin.ned@gmail.com>
2024-06-07 01:17:03 +0800

    fix: change env variable name for stock search api

--- src/app/stocks/search/page.jsx
+++ src/app/stocks/search/page.jsx
@@ -13,7 +13,7 @@ export default function Search() {
 			if (!searchTerm) return;
 			setLoading(true);
 			const response = await fetch(
-				`https://api.polygon.io/v3/reference/tickers?search=${searchTerm}&active=true&order=asc&limit=100&apiKey=${process.env.POLYGON_API_KEY}`
+				`https://api.polygon.io/v3/reference/tickers?search=${searchTerm}&active=true&order=asc&limit=100&apiKey=${process.env.NEXT_PUBLIC_POLYGON_API_KEY}`
 			);
 			const data = await response.json();
 			console.log(data.results);
