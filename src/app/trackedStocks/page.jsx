5e8ec91c2aae145e9007fa440097ceab00bcd5bd
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-06-10 14:32:27 +0800

    feat: create trackedstocks page

---
+++ src/app/trackedStocks/page.jsx
@@ -0,0 +1,24 @@
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import TrackedStocksList from "../(components)/TrackedStocksList";
+import { getServerSession } from "next-auth";
+
+export default async function TrackedStocksPage() {
+	const session = await getServerSession(authOptions);
+	console.log(session?.user?.email);
+	const stocks = await fetch(
+		`${process.env.NEXTAUTH_URL}/api/users/${session?.user?.email}/stocks`,
+		{
+			cache: "no-store",
+		}
+	).then((res) => res.json());
+
+	return (
+		<div className="w-screen flex justify-center">
+			<ul className="divide-y divide-gray-200 dark:divide-gray-700 w-full flex flex-row flex-wrap">
+				{stocks.map((stock) => (
+					<TrackedStocksList session={session} stock={stock} key={stock._id} />
+				))}
+			</ul>
+		</div>
+	);
+}
