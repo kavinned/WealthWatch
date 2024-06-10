5bbdbb1ccdc2ee6a09e905afc07a9b2adb2d1425
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-06-10 13:38:41 +0800

    feat: add api route for individual stock

---
+++ src/app/api/users/[email]/stocks/[sid]/route.js
@@ -0,0 +1,31 @@
+import { connectDB } from "../../../../../../../libs/mongodb";
+import { User } from "@/app/(models)/user";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+	try {
+		await connectDB();
+		const user = await User.findOne({ email: params.email });
+		const stock = user?.trackedStocks?.find(
+			(stock) => stock._id.toString() == params.sid
+		);
+		return NextResponse.json(stock, { status: 200 });
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function DELETE(request, { params }) {
+	try {
+		await connectDB();
+		const user = await User.findOne({ email: params.email });
+		const stock = user?.trackedStocks?.filter(
+			(stock) => stock._id.toString() != params.sid
+		);
+		user.trackedStocks = stock;
+		await user?.save();
+		return NextResponse.json(stock, { status: 200 });
+	} catch (error) {
+		console.log(error);
+	}
+}
