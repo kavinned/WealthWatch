012aa7cfff36e590860b565d4a990900444112db
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-06-10 13:11:32 +0800

    feat: link budget to view transactions page

--- src/app/(components)/BudgetInfo.jsx
+++ src/app/(components)/BudgetInfo.jsx
@@ -1,23 +1,26 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 
 export default function BudgetInfo({ budget }) {
 	return (
 		<li key={budget._id} className="py-3 sm:py-4">
-			<div className="flex items-center">
-				<div className="flex-shrink-0"></div>
-				<div className="flex-1 min-w-0 ms-4">
-					<p
-						onClick={() => console.log(budget)}
-						className="cursor-pointer text-lg font-semibold text-gray-900 truncate dark:text-white"
-					>
-						{budget.name}
-					</p>
+			<Link href={`/budgets/${budget._id}`}>
+				<div className="flex items-center">
+					<div className="flex-shrink-0"></div>
+					<div className="flex-1 min-w-0 ms-4">
+						<p
+							onClick={() => console.log(budget)}
+							className="cursor-pointer text-lg font-semibold text-gray-900 truncate dark:text-white"
+						>
+							{budget.name}
+						</p>
+					</div>
+					<div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+						${budget.limit}
+					</div>
 				</div>
-				<div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-					${budget.limit}
-				</div>
-			</div>
+			</Link>
 		</li>
 	);
 }
