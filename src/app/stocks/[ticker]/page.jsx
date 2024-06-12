4d122d37d7ec41e2d94605280535fed80480e4c5
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-06-12 17:06:45 +0800

    feat: change demo to api key

--- src/app/stocks/[ticker]/page.jsx
+++ src/app/stocks/[ticker]/page.jsx
@@ -4,23 +4,23 @@ import React from "react";
 
 export async function fetchStock(ticker) {
 	const res = await fetch(
-		`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo`
+		`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${process.env.NEXT_PUBLIC_ALPHAVANTAGE_API_KEY}`
 	);
 	return res?.json();
 }
 
 export async function fetchStockOverview(ticker) {
 	const res = await fetch(
-		`https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo`
+		`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${ticker}&apikey=${process.env.NEXT_PUBLIC_ALPHAVANTAGE_API_KEY}`
 	);
 	return res?.json();
 }
 
 export default async function StockDetails({ params }) {
-	const stock = await fetchStock(params.ticker);
-	const stockOverview = await fetchStockOverview(params.ticker);
-
-	console.log(stockOverview);
+	const [stock, stockOverview] = await Promise.all([
+		fetchStock(params.ticker),
+		fetchStockOverview(params.ticker),
+	]);
 
 	const latestPrice = stock["Time Series (Daily)"][
 		Object.keys(stock["Time Series (Daily)"])[0]
@@ -29,7 +29,7 @@ export default async function StockDetails({ params }) {
 	return (
 		<div className="w-full h-[calc(100dvh-7dvh)] overflow-auto">
 			<AddStockButton stock={stock} />
-			<div className="w-full h-fit flex flex-col justify-center items-center">
+			<div className="w-full h-fit flex flex-col justify-between items-center">
 				<h1 className="bg-zinc-800 px-3 py-2 text-2xl font-semibold text-zinc-300 rounded-xl drop-shadow-xl shadow-xl">
 					{stockOverview.Symbol}
 				</h1>
@@ -46,45 +46,60 @@ export default async function StockDetails({ params }) {
 					</span>
 				</p>
 			</div>
-			<div className="w-full h-fit flex justify-center items-center mt-3">
-				<div className="flex flex-col h-fit items-center justify-center w-1/2 gap-10 bg-zinc-200 rounded-lg p-10 shadow-2xl drop-shadow-2xl border-2 border-slate-400 border-opacity-50">
-					<h2 className="text-xl font-semibold text-zinc-700">
-						Prices for the last 30 days
-					</h2>
-					<Chart stock={stock} />
+			<div className="w-full flex flex-col justify-center items-center">
+				<div className="w-[90%] h-fit grid grid-cols-9 place-items-center place-content-center gap-5">
+					<div className="col-span-2 bg-zinc-200 rounded-lg p-3 shadow-2xl drop-shadow-2xl border-2 border-slate-400 border-opacity-50 w-full h-fit flex flex-col gap-5">
+						<p className="stock-overview ">
+							Analyst Target: <br />{" "}
+							<span className="bubble bg-blue-300">
+								${stockOverview.AnalystTargetPrice}
+							</span>
+						</p>
+						<p className="stock-overview">
+							52 Week High: <br />{" "}
+							<span className="bubble bg-emerald-300">
+								${stockOverview["52WeekHigh"]}
+							</span>
+						</p>
+						<p className="stock-overview">
+							52 Week Low: <br />{" "}
+							<span className="bubble bg-red-300">
+								${stockOverview["52WeekLow"]}
+							</span>
+						</p>
+						<p className="stock-overview">
+							50 Day Moving Average: <br />{" "}
+							<span className="bubble">
+								${stockOverview["50DayMovingAverage"]}
+							</span>
+						</p>
+					</div>
+					<div className="col-span-5 flex flex-col h-full items-center justify-center w-full gap-10 bg-zinc-200 rounded-lg p-3 shadow-2xl drop-shadow-2xl border-2 border-slate-400 border-opacity-50">
+						<h2 className="text-xl font-semibold text-zinc-700">
+							Prices for the last 30 days
+						</h2>
+						<Chart stock={stock} />
+					</div>
+					<div className="col-span-2 bg-zinc-200 rounded-lg p-3 shadow-2xl drop-shadow-2xl border-2 border-slate-400 border-opacity-50 w-full h-fit flex flex-col gap-5">
+						<p className="stock-overview">
+							Asset Type: <br />{" "}
+							<span className="bubble">{stockOverview.AssetType}</span>
+						</p>
+						<p className="stock-overview">
+							Exchange: <br />{" "}
+							<span className="bubble">{stockOverview.Exchange}</span>
+						</p>
+						<p className="stock-overview">
+							Country: <br />{" "}
+							<span className="bubble">{stockOverview.Country}</span>
+						</p>
+						<p className="stock-overview">
+							Sector: <br />{" "}
+							<span className="bubble">{stockOverview.Sector}</span> <br />
+							<span className="bubble">{stockOverview.Industry}</span>
+						</p>
+					</div>
 				</div>
-			</div>
-			<div>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nemo,
-					explicabo recusandae reprehenderit ut temporibus suscipit quis
-					reiciendis doloribus vitae dolorum optio aliquam labore aut
-					consectetur unde debitis laboriosam ipsam.
-				</p>
-			</div>
-			<div>
-				<p>
-					Ea, aspernatur ipsam distinctio, exercitationem laboriosam dolorum
-					quaerat ullam quos molestiae pariatur quia at tempora! Obcaecati,
-					veritatis, blanditiis architecto sed, quidem animi inventore omnis
-					sunt vero nemo corporis tempore asperiores.
-				</p>
-			</div>
-			<div>
-				<p>
-					Pariatur quae obcaecati reiciendis beatae optio quo maxime neque?
-					Facilis necessitatibus pariatur excepturi delectus suscipit culpa
-					dolor obcaecati magnam nostrum eum veniam, sunt ipsa saepe unde
-					recusandae impedit voluptatum veritatis.
-				</p>
-			</div>
-			<div>
-				<p>
-					Quas nesciunt fugit laboriosam vero placeat id illum beatae tempora
-					provident eius aperiam distinctio harum, qui voluptas, nobis pariatur!
-					Omnis repudiandae dolorum sed deleniti maiores aliquid modi odit
-					commodi voluptatem?
-				</p>
 			</div>
 		</div>
 	);
