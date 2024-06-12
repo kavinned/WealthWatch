209214c57f11908e5e82b1e0c854c56600370eb3
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-06-12 15:19:23 +0800

    feat: filter users list for not admin

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,12 +1,14 @@
 import { connectDB } from "../../../../libs/mongodb";
 import { User } from "@/app/(models)/user";
 import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET() {
 	try {
 		await connectDB();
 		const users = await User.find({});
-		return NextResponse.json(users, { status: 200 });
+		const usersNotAdmin = users.filter((user) => user.role !== "admin");
+		return NextResponse.json(usersNotAdmin, { status: 200 });
 	} catch (error) {
 		console.log(error);
 	}
