3a21b04e49455bdd778b05ee9bec29b4607d933a
kafeicheez2 <142488487+kafeicheez2@users.noreply.github.com>
2024-06-12 13:30:48 +0800

    feat: minor css changes and add date and time to dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -17,13 +17,24 @@ export default async function Dashboard() {
 				>
 					Welcome to your <br /> dashboard
 				</h1>
-				<h2
-					style={{ textShadow: "0px 4px 4px rgba(0, 0, 0, 0.4)" }}
-					className="dash-name text-emerald-800 font-bold text-2xl mr-5 mt-5 flex flex-col justify-around items-center "
+				<span className="flex flex-col">
+					<p className="text-4xl font-bold text-zinc-700">
+						{new Date().toLocaleTimeString(undefined, {
+							hour: "numeric",
+							minute: "numeric",
+						})}
+					</p>
+					<p className="text-md font-medium text-zinc-700 text-center">
+						{new Date().toDateString()}
+					</p>
+				</span>
+				<h3
+					style={{ textShadow: "0px 2px 2px rgba(0, 0, 0, 0.4)" }}
+					className="dash-name text-slate-700 font-bold text-lg mr-5 mt-5 flex flex-col justify-around items-center bg-zinc-200 p-4 border border-zinc-500 border-opacity-30 rounded-lg drop-shadow-lg shadow-sm"
 				>
 					{session?.user?.name}
 					<FaPiggyBank />
-				</h2>
+				</h3>
 			</span>
 			<div className="w-full h-full flex items-center justify-center gap-10 sm:p-10 xs:p-10">
 				<BudgetCard session={session} />
