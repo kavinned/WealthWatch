afbb20843ce6a8330b3118c2f1e6209acf6a6e57
Kavin <kavin.ned@gmail.com>
2024-10-11 01:33:10 +0800

    feat: add head and meta

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Quicksand } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "./providers";
 import NavBar from "./(components)/NavBar";
+import Head from "next/head";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 
@@ -14,6 +15,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<Head>
+				<meta http-equiv="x-ua-compatible" content="ie=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
 			<body className={quicksand.className}>
 				<AuthProvider>
 					<NavBar />
