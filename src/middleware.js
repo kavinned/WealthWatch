3fcfd106beb2afb09093995e129e4a177e5bff9f
Kavin <kavin.ned@gmail.com>
2024-10-19 17:52:43 +0800

    feat: change matcher to regex

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-	matcher: [
-		"/dashboard",
-		"/budgets",
-		"/budgets/:id",
-		"/budgets/:id/transactions",
-		"/budgets/:id/transactions/:tid",
-		"/budgets/:id/transactions/:tid/edit",
-		"/budgets/new",
-		"/stocks",
-		"/stocks/search",
-		"/admin",
-		"/admin/:email",
-		"/admin/:email/edit",
-	],
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico|signup|$).*)"],
 };
